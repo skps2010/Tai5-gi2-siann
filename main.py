b7951eaabee7e17faeb0c50c280dc2a8f8644d31
skps2010 <skps2010030908@yahoo.com>
2023-08-09 00:57:47 +0800

    add commands

--- main.py
+++ main.py
@@ -4,10 +4,22 @@ import os
 import requests
 import io
 import re
+from discord.ext import commands
+from pysondb import db
+import copy
 
 intents = discord.Intents.default()
 intents.message_content = True
-client = discord.Client(intents=intents)
+client = commands.Bot(command_prefix='$', intents=intents)
+database = db.getDb("database.json")
+DEFAULT_SETTING = {
+    'gid': -1,
+    'keyword': '[\[「\'"\(（][台臺]語[\)）\]」\'"]$',
+    'detect_tone': True,
+}
+tailo_r = re.compile(
+    r'[áàâǎāéèêěēíìîǐīóòôǒōúùûǔū]|ḿ|m̀|m̂|m̌|m̄|ń|ǹ|n̂|ň|n̄|ⁿ|a̍|e̍|ı̍|o̍|u̍',
+    flags=re.IGNORECASE)
 
 
 def get_sound_file(s: str):
@@ -24,6 +36,57 @@ def main():
     client.run(TOKEN)
 
 
+@client.command()
+async def taigi(ctx: commands.Context, *args):
+    if len(args) == 0 or args[0] == 'help':
+        await ctx.send(
+            '$taigi [ help | rule | set_keyword [regex] | set_detect_tone [bool] | reset ]',
+            reference=ctx.message)
+        return
+    cmd = args[0]
+    gid = ctx.guild.id
+    servers = database.getByQuery({"gid": gid})
+    is_found = len(servers) > 0
+    server = servers[0] if is_found else DEFAULT_SETTING
+
+    if cmd == 'rule':
+        await ctx.send(
+            f'keyword: {server["keyword"]}\ndetect_tone: {server["detect_tone"]}',
+            reference=ctx.message)
+
+    elif cmd == 'set_keyword' and len(args) > 1 and args[1] != '':
+        if is_found:
+            database.updateById(server['id'], {"keyword": args[1]})
+        else:
+            setting = copy.deepcopy(DEFAULT_SETTING)
+            setting['gid'] = gid
+            setting['keyword'] = args[1]
+            database.add(setting)
+        await ctx.send('修改成功', reference=ctx.message)
+
+    elif cmd == 'set_detect_tone' and len(args) > 1 and args[1] in ('True',
+                                                                    'False'):
+        if is_found:
+            database.updateById(server['id'],
+                                {"detect_tone": args[1] == 'True'})
+        else:
+            setting = copy.deepcopy(DEFAULT_SETTING)
+            setting['gid'] = gid
+            setting['detect_tone'] = args[1] == 'True'
+            database.add(setting)
+        await ctx.send('修改成功', reference=ctx.message)
+
+    elif cmd == 'reset':
+        if is_found:
+            database.deleteById(server['id'])
+        await ctx.send('修改成功', reference=ctx.message)
+
+    else:
+        await ctx.send(
+            '$taigi [ help | rule | set_keyword [regex] | set_detect_tone [bool] | reset ]',
+            reference=ctx.message)
+
+
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
@@ -34,13 +97,17 @@ async def on_message(message: discord.Message):
     if message.author == client.user:
         return
 
-    tailo_r = re.compile(
-        r'[áàâǎāéèêěēíìîǐīóòôǒōúùûǔū]|ḿ|m̀|m̂|m̌|m̄|ń|ǹ|n̂|ň|n̄|ⁿ|a̍|e̍|ı̍|o̍|u̍',
-        flags=re.IGNORECASE)
-    taigi_r = re.compile(r'[\[「\'"\(（][台臺]語[\)）\]」\'"]$')
+    await client.process_commands(message)
+
+    gid = message.guild.id
+    servers = database.getByQuery({"gid": gid})
+    server = servers[0] if len(servers) > 0 else DEFAULT_SETTING
+
+    taigi_r = re.compile(server['keyword'])
     content = message.content
 
-    if not tailo_r.search(content) and not taigi_r.search(content):
+    if not (server['detect_tone']
+            and tailo_r.search(content)) and not taigi_r.search(content):
         return
 
     content = taigi_r.sub('', content)
